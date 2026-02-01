ccc268e1e1e2861b9a5d9fbc98a8ac86225f033e
loza.dev <robertoloxa88@gmail.com>
2026-01-31 18:19:53 -0600

    feat: test

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -2,6 +2,6 @@ module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
         'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore']],
-        'subject-case': [2, 'always', 'sentence-case']
+        'subject-case': [0]
     }
 }
